f14ec958f27bf1d9bf947408116c67fede54db07
y.mudryi <y.mudryi@zinitsolutions.com>
2016-09-13 18:19:56 +0300

    second content start

---
+++ js/scrollimage.js
@@ -0,0 +1,12 @@
+/*(function($) {
+
+ $( document ).ready(function() {
+
+  $('.number_slide').waypoint(function() {
+			$('.number_slide').removeClass('active');
+			$(this.element).addClass('active');
+		},{offset:'30%'});
+  
+});
+
+})(jQuery)*/
