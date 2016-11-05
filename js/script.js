616432b117f171d6a5f5ab4eba17c4391e8b02b0
stavrzt <stavrzt@gmail.com>
2016-11-05 20:03:11 +0200

    3 pages done

--- js/script.js
+++ js/script.js
@@ -2,33 +2,33 @@
  $( document ).ready(function() {
 
 /*SCROLL IMAGES*/
-  $('.number_slide').waypoint(function() {
+  	$('.number_slide').waypoint(function() {
 		$('.number_slide').removeClass('active');
 		$(this.element).addClass('active');
 		$(".step").css({opacity: 0, transform: "rotate(0deg)"});
 		$(this.element).siblings().find(".step").css({opacity:1, "-webkit-transform": "rotate(360deg)", transform: "rotate(360deg)"});
 	},{offset:'45%'});
-/*SCROLL IMAGES*/
 
-/*SLIDER*/
+
 	$(function() {
-			var pull 		= jQuery('#pull');
-				menu 		= jQuery('nav ul');
-				menuHeight	= menu.height();
+		var pull 		= jQuery('#pull');
+			menu 		= jQuery('nav ul');
+			menuHeight	= menu.height();
 
-			jQuery(pull).on('click', function(e) {
-				e.preventDefault();
-				menu.slideToggle();
-			});
-
-			jQuery(window).resize(function(){
-        		var w = jQuery(window).width();
-        		if(w > 320 && menu.is(':hidden')) {
-        			menu.removeAttr('style');
-        		}
-    		});
+		jQuery(pull).on('click', function(e) {
+			e.preventDefault();
+			menu.slideToggle();
 		});
 
+		jQuery(window).resize(function(){
+        	var w = jQuery(window).width();
+        	if(w > 320 && menu.is(':hidden')) {
+        		menu.removeAttr('style');
+        	}
+    	});
+	});
+
+/*SLIDER*/
 	$(document).ready(function() {
 	$("#owl-example").owlCarousel({
 		navigation: true,
@@ -40,12 +40,40 @@
     	autoPlay:true
 		});
 	});
-	/*SLIDER*/
 
-	/*FULL-SIZE BANNER*/
+/*FULL-SIZE BANNER*/
+if(jQuery("div").is('#full_banner')){
 	history.scrollRestoration = 'manual';
 	document.getElementById('full_banner').scrollIntoView(true);
-	/*FULL-SIZE BANNER*/
+}
+
+/*ANIMATION MOUSE*/
+	function isScrolledIntoView(elem){
+		var docViewTop = $(window).scrollTop();
+		var docViewBottom = docViewTop + $(window).height();
+		var elemTop = $(elem).offset().top;
+		var elemBottom = elemTop + $(elem).height() - 150;
+		return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+	}
+
+	var state = false;
+	var refreshIntervalId;
+
+	$(window).scroll(function(){
+		if(jQuery("img").is('#mouse')){
+			if (isScrolledIntoView($("#mouse")) && !state){
+				refreshIntervalId = setInterval(function (){
+					$("#mouse").addClass("test");
+					setTimeout(function(){$("#mouse").removeClass("test");}, 700);
+				}, 1400);
+				state = true;
+			}
+			else if(!isScrolledIntoView($("#mouse"))){
+				clearInterval(refreshIntervalId);
+				state = false;
+			}
+		}
+	});
 });
 
 })(jQuery);
